b39a5117a65af1e093bcae7bf93cf41fb6805733
siswoo <juanmaldonado.co@gmail.com>
2021-10-27 22:33:39 -0500

    casa

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Sodaec.com</title>
+	<link rel="stylesheet" href="css/bootstrap.css">
+</head>
+<body>
+	<div class="col-12 text-center mt-3">
+		<img src="img/mant1.png" class="img-fluid">
+	</div>
+</body>
+</html>
+
+<script src="js/jquery-3.5.1.min.js"></script>
+<script src="js/bootstrap.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@9"></script>
